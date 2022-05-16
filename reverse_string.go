9549f20d3794f07563cb867bc5fcf6ea28c4b9eb
ARadzevich <Anastasiya_Radzevich@epam.com>
2022-05-16 22:17:07 +0400

    feat: add reverse string function

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	for i := 0; i < len(runes)/2; i++ {
+		x := len(runes) - i - 1
+		runes[i], runes[x] = runes[x], runes[i]
+	}
+	for _, v := range runes {
+		output += string(v)
+	}
 	return output
 }
